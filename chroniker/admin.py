0530cc84bad00d7c6c8aa031c38beb401c08c4a0
Vince Veselosky <vince.veselosky@draft2digital.com>
2022-06-03 09:21:32 -0400

    WEB-2718 Removed in 4.0 django.conf.urls.url

--- chroniker/admin.py
+++ chroniker/admin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.contrib import admin
 from django.core.management import get_commands
 from django.urls import reverse, NoReverseMatch
